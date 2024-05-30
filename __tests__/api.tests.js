1ca034a377ef16aec01772dfb1d5a28ffa2a9136
Liam Houghton <liamhoughton@Liams-MacBook-Pro.local>
2024-05-30 09:18:32 +0100

    update packages to resolve open handle issues with tests

--- __tests__/api.tests.js
+++ __tests__/api.tests.js
@@ -1,10 +1,21 @@
 const request = require("supertest");
 const app = require("../app");
 const endpoints = require('../endpoints.json')
+const db = require('../db/connection');
+const seed = require('../db/seeds/seed');
+const testData = require('../db/data/test-data/index')
+
+beforeEach(() => {
+    return seed(testData)
+})
+
+afterAll(() => {
+    return db.end()
+})
 
 describe("/api", () => {
   it('should return the endpoints', async () => {
-    const { text, statusCode } = await request(app).get("/api");
+    const { text } = await request(app).get("/api");
     const response = JSON.parse(text);
     expect(response).toEqual(endpoints)
   });
